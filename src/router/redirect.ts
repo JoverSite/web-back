c15c39a1b320e76845c794cf08f952fce6b27546
JoverZhang <joverzh@gmail.com>
2020-05-26 17:23:52 +0800

    fix(redirect): add new prefix.

--- src/router/redirect.ts
+++ src/router/redirect.ts
@@ -3,10 +3,11 @@ import koaRouter from 'koa-router'
 const router = new koaRouter()
 
 // const prefix = 'https://raw.githubusercontent.com/JoverSite/web-front/package/dist'
-const prefix2 = 'https://cdn.jsdelivr.net/gh/JoverSite/web-front@package/dist/'
+// const prefix2 = 'https://cdn.jsdelivr.net/gh/JoverSite/web-front@package/dist/'
+const prefix3 = 'https://joversite.gitee.io/web-front'
 
 router.get(new RegExp('(.*)'), async (ctx) => {
-  ctx.redirect(prefix2 + ctx.params[0])
+  ctx.redirect(prefix3 + ctx.params[0])
 })
 
 export default router
