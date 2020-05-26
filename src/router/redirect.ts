bd504127cf4610cdddd82aad982aef8070c14196
JoverZhang <joverzh@gmail.com>
2020-05-26 16:05:56 +0800

    feat(redirect): add prefix2.

--- src/router/redirect.ts
+++ src/router/redirect.ts
@@ -2,10 +2,11 @@ import koaRouter from 'koa-router'
 
 const router = new koaRouter()
 
-const prefix = 'https://raw.githubusercontent.com/JoverSite/web-front/package/dist'
+// const prefix = 'https://raw.githubusercontent.com/JoverSite/web-front/package/dist'
+const prefix2 = 'https://cdn.jsdelivr.net/gh/JoverSite/web-front@package/dist/'
 
 router.get(new RegExp('(.*)'), async (ctx) => {
-  ctx.redirect(prefix + ctx.params[0])
+  ctx.redirect(prefix2 + ctx.params[0])
 })
 
 export default router
